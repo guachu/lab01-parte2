05eda164a5881ce7185226684a76e7b012af9e7b
Alejandra Gómez <alejita.9813.dg@gmail.com>
2019-01-21 23:24:10 -0500

    parte 2 finalizada sin README

---
+++ src/main/java/edu/eci/App.java
@@ -0,0 +1,15 @@
+package edu.eci;
+
+/**
+ * Hello world!
+ *
+ */
+import edu.eci.FileSpy;
+
+public class App 
+{
+    public static void main( String[] args )throws Exception
+    {
+        FileSpy.main(args);
+    }
+}
